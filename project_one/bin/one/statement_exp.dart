534d464414155a0aca9637920622a8b2926d2cfd
ChineduG <investorgbriel@gmail.com>
2022-03-03 09:40:02 +0100

    Mathematical functions in Dart

--- project_one/bin/one/statement_exp.dart
+++ project_one/bin/one/statement_exp.dart
@@ -6,10 +6,12 @@ void main(){
   // Expressions
   4;
   "Hey there";
-
+  // Arithmetic Expression
   print(1 + 1);
   print(3 * 3);
   print(4 - 3);
   print(4 / 3);
+  
+  // Integer Expression
   print(4 ~/ 3);
 }
